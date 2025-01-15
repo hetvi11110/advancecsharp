169652e01d3f901af0aff09a0571004e092e372b
hetvi11110 <hetvi.patel.585112@gmail.com>
2025-01-14 20:47:39 -0500

    feat: que11

---
+++ Question11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Question11
+{
+    public class Program
+    {
+        public int GenerateNumber()
+        {
+            Random number = new Random();
+            return number.Next(1, 10001);
+        }
+
+        public string PrintNumber(int randomNumber)
+        {
+            return $"The generated number is: {randomNumber}";
+        }
+
+        static void Main()
+        {
+            Program program = new Program();
+
+            Task task = Task.Factory.StartNew(program.GenerateNumber)
+                .ContinueWith(taskResult =>
+                {
+                    string result = program.PrintNumber(taskResult.Result);
+                    Console.WriteLine(result);
+                });
+
+            task.Wait();
+        }
+    }
+}
